c58e9ceb9fa104935387738e37d748d65984ec45
Cedrick <cedrickdavidvernon@gmail.com>
2023-05-02 10:57:35 -0400

    First commit

--- app/src/main/java/com/example/qrcodeapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/qrcodeapp/ui/home/HomeViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.qrcodeapp.ui.home
 
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : Fragment() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Home Fragment"
     }
     val text: LiveData<String> = _text
 }
